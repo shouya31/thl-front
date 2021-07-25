9b7811d0be2efce1cb293ce05a5a31b9adf41150
栗田ひかり <49625998+kuritahikari@users.noreply.github.com>
2021-07-25 15:34:31 +0900

    レスポンシブ_TOP_ヘッダー差し替え
    Tab/スマホ表示のヘッダーをデザインカンプ通りに差し替えました！
    
    その他、前回の未対応箇所を完了しました▼
    ・1024px,980pxあたりで軽度の崩れあり
    ・js　画面幅に応じたif文を書いていない
    ・ファーストビューのキャッチコピーが常に左右中央にきていない
    ⇨こちらについて、左右中央にはしていないです。
    理由：デザイン的に左寄りのほうが整っていると判断したため。

--- js/script.js
+++ js/script.js
@@ -1,33 +1,32 @@
-$(function () {
-
-    // ヘッダースクロール
-    const headerOffset = $(".header").offset().top;
-    const headerHeight = $(".header").height();
-    const changeLine = headerOffset + headerHeight;
-
-    $(window).scroll(function () {
-        const scrollHeight = $(this).scrollTop();
-        if (scrollHeight > changeLine) {
-            $("header").addClass("js-scroll");
-        } else {
-            $("header").removeClass("js-scroll");
-        }
-
-    })
-})
-
-$(function () {
-    $('.zdo_drawer_button').click(function () {
-      $(this).toggleClass('active');
-      $('.header__body').fadeToggle();
-    })
-  })
-  $(function () {
-    $('.header__global-nav-link').click(function () {
-      $('.header__body').fadeOut();
-      $('.zdo_drawer_button').toggleClass('active');
-    })
-  })
-
-
+$(window).on('load resize', function(){
+    　if (window.matchMedia('(min-width: 960px)').matches){
+        $(function () {
+            // ヘッダースクロール
+            const headerOffset = $(".header").offset().top;
+            const headerHeight = $(".header").height();
+            const changeLine = headerOffset + headerHeight;
+            $(window).scroll(function () {
+                const scrollHeight = $(this).scrollTop();
+                if (scrollHeight > changeLine) {
+                    $("header").addClass("js-scroll");
+                } else {
+                    $("header").removeClass("js-scroll");
+                }
+            })
+        })
+    　} else {(window.matchMedia('(max-width: 959px)').matches)
+        $(function () {
+            $('.zdo_drawer_button').click(function () {
+              $(this).toggleClass('active');
+              $('.header__global-nav').fadeToggle();
+            })
+          })
+          $(function () {
+            $('.header__global-nav-link').click(function () {
+              $('.header__global-nav').fadeOut();
+              $('.zdo_drawer_button').toggleClass('active');
+            })
+          })
+    　}
+    });
 
