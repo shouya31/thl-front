ad3a08d51447fcb3d99266c1aaf5c8031c606b23
栗田ひかり <49625998+kuritahikari@users.noreply.github.com>
2021-07-25 16:25:54 +0900

    コンフリクト解消_jsも
    フッター微調整しました

--- js/script.js
+++ js/script.js
@@ -31,28 +31,3 @@ $(window).on('load resize', function(){
     　}
     });
 
-$(function () {
-
-    // ヘッダースクロール
-    const headerOffset = $(".header").offset().top;
-    const headerHeight = $(".header").height();
-    const changeLine = headerOffset + headerHeight;
-
-    $(window).scroll(function () {
-        const scrollHeight = $(this).scrollTop();
-        if (scrollHeight > changeLine) {
-            $("header").addClass("js-scroll");
-        } else {
-            $("header").removeClass("js-scroll");
-        }
-
-    })
-
-    $('.burger-btn').on('click', function () {
-        $('.burger-btn').toggleClass('close');
-        $('.nav-wrapper').fadeToggle(500);
-        $('body').toggleClass('noscroll');
-    });
-})
-
-
