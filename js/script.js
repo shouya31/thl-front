9eadfc5519ab93c278c59df342175ea40cc3e739
栗田ひかり <49625998+kuritahikari@users.noreply.github.com>
2021-07-26 19:26:09 +0900

    レスポンシブ作業_2点修正
    下記対応いたしました。
    ・メニューの表示・非表示が繰り返される挙動
    ・Safariで確認すると、横にスライドできる（私たちのビジョン」セクションにある背景画像が原因）

--- js/script.js
+++ js/script.js
@@ -1,33 +1,45 @@
+$(function(){
+  var timer = false;
 
-$(window).on('load resize', function(){
-    　if (window.matchMedia('(min-width: 960px)').matches){
-        $(function () {
-            // ヘッダースクロール
-            const headerOffset = $(".header").offset().top;
-            const headerHeight = $(".header").height();
-            const changeLine = headerOffset + headerHeight;
-            $(window).scroll(function () {
-                const scrollHeight = $(this).scrollTop();
-                if (scrollHeight > changeLine) {
-                    $("header").addClass("js-scroll");
-                } else {
-                    $("header").removeClass("js-scroll");
-                }
-            })
+  $(window).resize(function() {
+      if (timer !== false) {
+          clearTimeout(timer);
+      }
+      timer = setTimeout(function() {
+      location.reload();
+      }, 200);
+  })
+
+  var windowWidth = $(window).width();
+  var windowSm = 960;
+  if (windowWidth <= windowSm) {
+    $(function () {
+      $('.zdo_drawer_button').click(function () {
+        $(this).toggleClass('active');
+        $('.header__global-nav.sp').fadeToggle();
+      })
+    }),
+      $(function () {
+        $('.header__global-nav-link.sp').click(function () {
+          $('.header__global-nav.sp').fadeOut();
+          $('.zdo_drawer_button').toggleClass('active');
         })
-    　} else {(window.matchMedia('(max-width: 959px)').matches)
-        $(function () {
-            $('.zdo_drawer_button').click(function () {
-              $(this).toggleClass('active');
-              $('.header__global-nav').fadeToggle();
-            })
-          })
-          $(function () {
-            $('.header__global-nav-link').click(function () {
-              $('.header__global-nav').fadeOut();
-              $('.zdo_drawer_button').toggleClass('active');
-            })
-          })
-    　}
-    });
+      })
+  } else {
+    $(function () {
+      // ヘッダースクロール
+      const headerOffset = $(".header").offset().top;
+      const headerHeight = $(".header").height();
+      const changeLine = headerOffset + headerHeight;
+      $(window).scroll(function () {
+        const scrollHeight = $(this).scrollTop();
+        if (scrollHeight > changeLine) {
+          $("header").addClass("js-scroll");
+        } else {
+          $("header").removeClass("js-scroll");
+        }
 
+      })
+    })
+  }
+});
